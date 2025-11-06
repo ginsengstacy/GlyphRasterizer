ebe7c40062407a4e5dfcfe2eb83b6ae68085ae19
ginsengstacy <autbaneaccro@gmail.com>
2025-11-06 19:16:15 +0100

    Change MinImageSize from 1 to 16

--- src/GlyphRasterizer/Configuration/AppConfig.cs
+++ src/GlyphRasterizer/Configuration/AppConfig.cs
@@ -7,7 +7,7 @@ namespace GlyphRasterizer.Configuration;
 public static class AppConfig
 {
     public static readonly PixelFormat PixelFormat = PixelFormats.Pbgra32;
-    public const int MinImageSize = 1;
+    public const int MinImageSize = 16;
     public const int MaxImageSize = 2048;
     public const int MaxIcoSize = 256;
 
